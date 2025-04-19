58435a9e7a073fec55719ace8ad4dd0c15744ba9
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2025-04-19 14:28:20 -0400

    feat: switch to nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       enabled: true,
     },
   },
-  modules: ["@nuxtjs/tailwindcss", "vue3-carousel-nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "vue3-carousel-nuxt", "@nuxt/fonts"],
   extends: ["./drop-base"],
   css: ["~/assets/core.scss"],
   app: {
@@ -18,6 +18,72 @@ export default defineNuxtConfig({
   },
   carousel: {
     prefix: "Vue",
+  },
+
+  nitro: {
+    minify: true,
+    compressPublicAssets: true,
+  },
+
+  fonts: {
+    families: [
+      {
+        name: "Helvetica",
+        src: "/fonts/helvetica/helvetica-300-light.woff2",
+      },
+      {
+        name: "Helvetica",
+        src: "/fonts/helvetica/Helvetica-400-italic.woff",
+      },
+      {
+        name: "Helvetica",
+        src: "/fonts/helvetica/Helvetica-400.woff",
+      },
+      {
+        name: "Helvetica",
+        src: "/fonts/helvetica/Helvetica-600-bold-italic.woff",
+      },
+      {
+        name: "Helvetica",
+        src: "/fonts/helvetica/Helvetica-600-bold.woff",
+      },
+      {
+        name: "Inter",
+        src: "/fonts/inter/Inter-variable-400.ttf",
+      },
+      {
+        name: "Inter",
+        src: "/fonts/inter/Inter-variable-400-italic.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-100-thin.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-300-light.woff.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-400-regular.woff.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-500-medium.woff.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-600-bold.woff.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-700-extra-bold.ttf",
+      },
+      {
+        name: "Motiva Sans",
+        src: "/fonts/motiva/motiva-sans-900-black.woff.ttf",
+      },
+    ],
   },
 
   typescript: {
