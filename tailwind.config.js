0984dd17725e6aa48555ceab5a7e79682dadc25a
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2025-04-19 20:08:42 -0400

    feat: add fallback fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter"],
-        display: ["Motiva Sans"],
+        sans: ["Inter", "San Francisco", "sans-serif"],
+        display: ["Motiva Sans", "Arial", "sans-serif"],
       },
       colors: {
         zinc: {
