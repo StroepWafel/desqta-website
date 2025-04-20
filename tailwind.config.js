84e952ec55eaa01076e4dad5dd642038f4c78636
DecDuck <declanahofmeyr@gmail.com>
2025-04-20 23:39:40 +1000

    fix: remove fallback fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", "San Francisco", "sans-serif"],
-        display: ["Motiva Sans", "Arial", "sans-serif"],
+        sans: ["Inter", "sans-serif"], //"San Francisco",
+        display: ["Motiva Sans", "sans-serif"], //"Arial",
       },
       colors: {
         zinc: {
