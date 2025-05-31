2f0033cf1c0c72cd3cd773612740e58296874c20
Aden Lindsay <lindsaya542@gmail.com>
2025-05-31 20:33:22 +0930

    chore: build from ground app

--- components/ComingSoon.vue
+++
@@ -1,3 +0,0 @@
-<template>
-      <span class="inline-flex items-center rounded-md bg-blue-600/10 px-2 py-1 text-xs font-medium text-blue-400 ring-1 ring-blue-600/40 ring-inset">Coming Soon&trade;</span>
-</template>
