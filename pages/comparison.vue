999a91bd0f46b4e06e545cb2b7889cbbfedc6b8b
StroepWafel <109832156+StroepWafel@users.noreply.github.com>
2025-08-07 16:30:24 +0930

    Update comparison.vue

--- pages/comparison.vue
+++ pages/comparison.vue
@@ -100,7 +100,7 @@
             href="/download"
             class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-all duration-200"
           >
-            Download Now
+            Download now
           </NuxtLink>
         </div>
       </div>
